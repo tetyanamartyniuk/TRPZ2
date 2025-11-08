0bb5eb8b1b87fb2a75836293b51933ced4fa9581
tetyanamartyniuk <145157063+tetyanamartyniuk@users.noreply.github.com>
2025-11-08 13:34:14 +0200

    builder pattern

--- src/main/HttpResponse.java
+++ src/main/HttpResponse.java
@@ -3,15 +3,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpResponse {
-    private int statusCode;
-    private Map<String, String> headers = new HashMap<>();
-    private String body;
 
-    public HttpResponse(int statusCode, String body) {
-        this.statusCode = statusCode;
-        this.body = body;
+        private final String status;
+        private final Map<String, String> headers;
+        private final String body;
+
+        public HttpResponse(String status, Map<String, String> headers, String body) {
+            this.status = status;
+            this.headers = headers;
+            this.body = body;
+        }
     }
-
-    public int getStatusCode() { return statusCode; }
-    public String getBody() { return body; }
-}
---
+++ src/main/HttpResponseDirector.java
@@ -0,0 +1,56 @@
+package src.main;
+
+import java.util.Map;
+public class HttpResponseDirector {
+
+    private static final HttpResponseBuilder builder = new HttpResponseBuilder();
+
+    public static HttpResponse Ok(String body, Map<String, String> headers) {
+        builder.buildStatus("200 OK");
+        builder.buildHeaders(headers);
+        builder.buildBody(body);
+        return builder.build();
+    }
+
+    public static HttpResponse NotFound(Map<String, String> headers) {
+        builder.buildStatus("404 Not Found");
+        builder.buildHeaders(headers);
+        builder.buildBody(null);
+        return builder.build();
+    }
+
+    public static HttpResponse InternalServerError(Map<String, String> headers) {
+        builder.buildStatus("500 Internal Server Error");
+        builder.buildHeaders(headers);
+        builder.buildBody(null);
+        return builder.build();
+    }
+
+    public static HttpResponse BadRequest(Map<String, String> headers) {
+        builder.buildStatus("400 Bad Request");
+        builder.buildHeaders(headers);
+        builder.buildBody(null);
+        return builder.build();
+    }
+
+    public static HttpResponse Created(String body, Map<String, String> headers) {
+        builder.buildStatus("201 Created");
+        builder.buildHeaders(headers);
+        builder.buildBody(body);
+        return builder.build();
+    }
+
+    public static HttpResponse NoContent(Map<String, String> headers) {
+        builder.buildStatus("204 No Content");
+        builder.buildHeaders(headers);
+        builder.buildBody(null);
+        return builder.build();
+    }
+
+    public static HttpResponse Forbidden(Map<String, String> headers) {
+        builder.buildStatus("403 Forbidden");
+        builder.buildHeaders(headers);
+        builder.buildBody(null);
+        return builder.build();
+    }
+}
